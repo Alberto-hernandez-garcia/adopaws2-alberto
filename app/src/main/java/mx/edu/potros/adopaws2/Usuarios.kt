41abc4e4b0dd216852be29785698c62d2844303d
Alberto Hernandez <alberto.hernandez@potros.itson.edu.mx>
2023-05-15 17:30:23 -0700

    login de adopaws agregado

---
+++ app/src/main/java/mx/edu/potros/adopaws2/Usuarios.kt
@@ -0,0 +1,5 @@
+package mx.edu.potros.adopaws2
+
+data class Usuarios(val nomusuario: String? = null, val fechanac: String? = null, val correo: String? = null, val contrasenia: String? = null){
+
+}
